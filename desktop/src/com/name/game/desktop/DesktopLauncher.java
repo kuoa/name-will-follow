67efbbe6dd1673de8136ad39235f753a974f7ac9
kuoa <postarusandu@gmail.com>
2016-05-28 21:34:55 +0200

    Add project base

---
+++ desktop/src/com/name/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.name.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.name.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
